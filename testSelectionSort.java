d69ead9c29d3e8f8c04c8a910ced1131c59f7e86
Ben Hong <benhong@umich.edu>
2023-11-30 19:18:47 -0500

    initial

---
+++ testSelectionSort.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class testSelectionSort {
+    @Test
+    public void test() {
+        testPositive();
+        testNegative();
+        testMixed();
+        testDuplicates();
+    }
+
+    public testSelectionSort() {
+    }
+
+    public void testPositive() {
+        int[] arr = {8, 9, 7, 10, 2};
+        int[] Sortedarr = {2, 7, 8, 9, 10};
+        SelectionSort sorter = new SelectionSort();
+        assertArrayEquals(Sortedarr, sorter.basicSelectionSort(arr));
+    }
+
+    public void testNegative() {
+        int[] arr = {-3, -1, -4, -5, -2};
+        int[] Sortedarr = {-5, -4, -3, -2, -1};
+        SelectionSort sorter = new SelectionSort();
+        assertArrayEquals(Sortedarr, sorter.basicSelectionSort(arr));
+    }
+
+    public void testMixed() {
+        int[] arr = {-2, 3, 0, -1, 5};
+        int[] Sortedarr = {-2, -1, 0, 3, 5};
+        SelectionSort sorter = new SelectionSort();
+        assertArrayEquals(Sortedarr, sorter.basicSelectionSort(arr));
+    }
+
+    public void testDuplicates() {
+        int[] arr = {2, 3, 2, -1, -1};
+        int[] Sortedarr = {-1, -1, 2, 2, 3};
+        SelectionSort sorter = new SelectionSort();
+        assertArrayEquals(Sortedarr, sorter.basicSelectionSort(arr));
+    }
+}
